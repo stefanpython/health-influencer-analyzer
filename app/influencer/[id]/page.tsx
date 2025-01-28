9097e63e9e8483650941bc8402d97e5bc1a9cb90
stefanpython <stefanhtml@yahoo.com>
2025-01-28 13:26:12 +0200

    Create folder structure for the project

---
+++ app/influencer/[id]/page.tsx
@@ -0,0 +1,13 @@
+const InfluencerPage = () => {
+  // Use the 'id' to fetch data for the specific influencer
+  // (e.g., from an API, database, or static file)
+
+  return (
+    <div>
+      <h1>Influencer:</h1>
+      {/* Display influencer information here */}
+    </div>
+  );
+};
+
+export default InfluencerPage;
